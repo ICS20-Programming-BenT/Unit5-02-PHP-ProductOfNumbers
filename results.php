582f0c06459ff39628e931d1c6658b79cc54c075
BenThomson2007 <124379717+BenThomson2007@users.noreply.github.com>
2023-05-11 14:03:42 +0000

    Fixed errors

--- results.php
+++ results.php
@@ -1,40 +1,39 @@
 <?php
 
-  // Initializing variable for product as zero
-  $product = 0;
+// Initializing variable for product as zero
+$product = 0;
 
-  // Getting the two numbers from the user that will be used to calculate the product
-  $firstNum = intval($_POST["first-number"]);
-  $secondNum = intval($_POST["second-number"]);
+// Getting the two numbers from the user that will be used to calculate the product
+$firstNum = intval($_POST["first-number"]);
+$secondNum = intval($_POST["second-number"]);
 
-  // Removing any negative signs from numbers in order to be able to calculate using repeated addition
-  $absoluteFirstNum = abs($firstNum);
-  $absoluteSecondNum = abs($secondNum);
+// Removing any negative signs from numbers in order to be able to calculate using repeated addition
+$absoluteFirstNum = abs($firstNum);
+$absoluteSecondNum = abs($secondNum);
 
-  // If the user does not enter their min and max, display that they must enter both numbers
-  if (empty($firstNum) || empty($secondNum)) {
-      echo "Please enter integers in both text fields.";
-  } 
+// If the user does not enter their min and max, display that they must enter both numbers
+if (empty($firstNum) || empty($secondNum)) {
+    echo "Please enter integers in both text fields.";
+}
 
-  // Otherwise, continue the function to calculate the product
-  else {
-      // For loop to do repeated addition to find the product
-      for ($counter = 0; $counter < $absoluteSecondNum; $counter++) {
-          $product += $absoluteFirstNum;
-      }
+// Otherwise, continue the function to calculate the product
+else {
+    // For loop to do repeated addition to find the product
+    for ($counter = 0; $counter < $absoluteSecondNum; $counter++) {
+        $product += $absoluteFirstNum;
+    }
 
-      // If statements: Reapplying the negative sign if either the first or second number is negative
-      if ($firstNum < 0) {
-          $product * -1;
-      }
+    // If statements: Reapplying the negative sign if either the first or second number is negative
+    if ($firstNum < 0) {
+        $product * -1;
+    }
 
-      if ($secondNum < 0) {
-          $product * -1;
-      }
+    if ($secondNum < 0) {
+        $product * -1;
+    }
 
-      // Displaying the product to the screen
-      echo "The product of your selected two numbers is " . $product . ".";
-  }
+    // Displaying the product to the screen
+    echo "The product of your selected two numbers is " . $product . ".";
+}
 
 ?>
-
